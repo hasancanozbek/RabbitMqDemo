f74ea365acfc6183838789d975d7c86d5eba8551
Hasan Can Ozbek <hasancanozbekk@gmail.com>
2023-02-03 00:18:04 +0300

    Consume basic message

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello world");
+﻿//create connection
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ();
+factory.HostName = "localhost";
+
+//activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel ();
+
+//create queue
+channel.QueueDeclare(queue: "example-queue", exclusive:false);
+/*
+ * The consumer's queue and publisher's queue must be in the same configuration. 
+ */
+
+//read message from queue
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume("example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    /*
+     * queued message will be processed here
+     * e.Body : get in the queue message's complete data 
+     * e.Body.Span or e.Body.ToArray() : get in the queue message's byte data
+     */
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -10,13 +10,13 @@ using IConnection connection = factory.CreateConnection ();
 using IModel channel = connection.CreateModel();
 
 //create queue
-channel.QueueDeclare(queue:"exapmle-queue", exclusive:false );
+channel.QueueDeclare(queue: "example-queue", exclusive:false );
 
 //send message to queue
 /*
  * RabbitMQ accept messages only byte type. So we have to convert messages to bytes.
  */
 byte[] message = Encoding.UTF8.GetBytes("Hello world!");
-channel.BasicPublish(exchange:"", routingKey: "exapmle-queue", body:message);
+channel.BasicPublish(exchange:"", routingKey: "example-queue", body:message);
 
 Console.Read();
