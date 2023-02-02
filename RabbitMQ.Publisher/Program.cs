bbeaa7ff38f6a7109ac44f35b0766cc439e93298
Hasan Can Ozbek <hasancanozbekk@gmail.com>
2023-02-02 23:47:15 +0300

    Basic publisher's message created.

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//create connection
+ConnectionFactory factory = new ();
+factory.HostName= "localhost";
+
+//activate connection and create channel
+using IConnection connection = factory.CreateConnection ();
+using IModel channel = connection.CreateModel();
+
+//create queue
+channel.QueueDeclare(queue:"exapmle-queue", exclusive:false );
+
+//send message to queue
+/*
+ * RabbitMQ accept messages only byte type. So we have to convert messages to bytes.
+ */
+byte[] message = Encoding.UTF8.GetBytes("Hello world!");
+channel.BasicPublish(exchange:"", routingKey: "exapmle-queue", body:message);
+
+Console.Read();
