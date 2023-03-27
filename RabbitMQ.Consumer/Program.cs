8ee457a5120c84fbad2929a7abd5704e294352ea
Hasan Can Ozbek <hasancanozbekk@gmail.com>
2023-03-27 04:02:45 +0300

    Direct exhange

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -10,26 +10,40 @@ factory.HostName = "localhost";
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue:queueName, exchange:"direct-exchange-example", routingKey:"direct-queue-example");
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
 //create queue
-channel.QueueDeclare(queue: "example-queue", exclusive: false, durable:true);
+//channel.QueueDeclare(queue: "example-queue", exclusive: false, durable:true);
 /*
  * The consumer's queue and publisher's queue must be in the same configuration.
  */
 
 //read message from queue
-EventingBasicConsumer consumer = new(channel);
-var consumerTag = channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+//EventingBasicConsumer consumer = new(channel);
+//var consumerTag = channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
 
-channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global:true) ;
-consumer.Received += (sender, e) =>
+//channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global:true) ;
+//consumer.Received += (sender, e) =>
 {
     /*
      * queued message will be processed here
      * e.Body : get in the queue message's complete data 
      * e.Body.Span or e.Body.ToArray() : get in the queue message's byte data
      */
-    Thread.Sleep(1000);
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    //Thread.Sleep(1000);
+    //Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
 
     /*
      * multiple : send signal for all messages sent before the processed message
@@ -42,7 +56,7 @@ consumer.Received += (sender, e) =>
     //channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
     //channel.BasicNack(deliveryTag: e.DeliveryTag, multiple: false, requeue: false);
     //channel.BasicCancel(consumerTag: consumerTag);
-    channel.BasicReject(deliveryTag: 3, requeue: true);
+    //channel.BasicReject(deliveryTag: 3, requeue: true);
 };
 
 Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -9,20 +9,34 @@ factory.HostName = "localhost";
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-//create queue
-channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
-IBasicProperties properties = channel.CreateBasicProperties();
-properties.Persistent = true;
+////create queue
+//channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+//IBasicProperties properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
 
 //send message to queue
 /*
  * RabbitMQ accept messages only byte type. So we have to convert messages to bytes.
  */
-for (int i = 1; i < 100; i++)
+
+//for (int i = 1; i < 100; i++)
+//{
+//    byte[] message = Encoding.UTF8.GetBytes($"Hello world - {i}");
+//    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
+//    Console.WriteLine($"Message {i} send");
+//}
+
+channel.ExchangeDeclare(exchange:"direct-exchange-example", type:ExchangeType.Direct);
+
+while (true)
 {
-    byte[] message = Encoding.UTF8.GetBytes($"Hello world - {i}");
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
-    Console.WriteLine($"Message {i} send");
+    Console.Write("Message : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange:"direct-exchange-example",
+        routingKey:"direct-queue-example",
+        body:byteMessage);
 }
 
 Console.Read();
